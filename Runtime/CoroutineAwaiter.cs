1ce8ea6368337a44f08ce93301fb33e7a2bd7b78
vikle <ialucard@yandex.ru>
2024-07-26 13:18:22 +0500

    upload src

---
+++ Runtime/CoroutineAwaiter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace AwaitableCoroutines
+{
+    public sealed class CoroutineAwaiter : ICriticalNotifyCompletion
+    {
+        public IEnumerator Coroutine { [MethodImpl(MethodImplOptions.AggressiveInlining)]get; }
+        public YieldInstruction Instruction { [MethodImpl(MethodImplOptions.AggressiveInlining)]get; }
+
+        readonly CoroutineAwaiterEnumerator m_controller;
+        
+        Action m_continuation;
+        bool m_isCompleted;
+
+        public bool IsCompleted
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]get => m_isCompleted;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]set
+            {
+                m_isCompleted = value;
+                if (value == false) return;
+                m_continuation?.Invoke();
+                m_continuation = null;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private CoroutineAwaiter()
+        {
+            m_controller = new CoroutineAwaiterEnumerator(this);
+            AwaitableCoroutineRunner.StartExternalCoroutine(m_controller);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public CoroutineAwaiter(IEnumerator enumerator) : this()
+        {
+            Coroutine = enumerator;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public CoroutineAwaiter(YieldInstruction instruction) : this()
+        {
+            Instruction = instruction;
+        }
+        
+        public void OnCompleted(Action continuation)
+        {
+            m_continuation = continuation;
+        }
+
+        public void UnsafeOnCompleted(Action continuation)
+        {
+            m_continuation = continuation;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public object GetResult()
+        {
+            return m_controller.Current;
+        }
+    };
+    
+    public static class CoroutineAwaiterExtensions
+    {
+        public static CoroutineAwaiter GetAwaiter(this IEnumerator coroutine)
+        {
+            return new CoroutineAwaiter(coroutine);
+        }
+
+        public static CoroutineAwaiter GetAwaiter(this YieldInstruction instruction)
+        {
+            return new CoroutineAwaiter(instruction);
+        }
+    };
+}
---
+++ Runtime/CoroutineAwaiterEnumerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace AwaitableCoroutines
+{
+    public sealed class CoroutineAwaiterEnumerator : IEnumerator
+    {
+        readonly CoroutineAwaiter m_awaiter;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public CoroutineAwaiterEnumerator(CoroutineAwaiter awaiter)
+        {
+            m_awaiter = awaiter;
+        }
+
+        public object Current
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]get;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]private set;
+        }
+
+        public bool MoveNext()
+        {
+            var awaiter = m_awaiter;
+            var coroutine = awaiter.Coroutine;
+
+            if (coroutine != null)
+            {
+                bool result = coroutine.MoveNext();
+                Current = coroutine.Current;
+                awaiter.IsCompleted = !result;
+                return result;
+            }
+
+            if (Current == null)
+            {
+                Current = awaiter.Instruction;
+                return true;
+            }
+
+            awaiter.IsCompleted = true;
+            return false;
+        }
+
+        public void Reset()
+        {
+            Current = null;
+            m_awaiter.IsCompleted = false;
+        }
+    };
+}
